d93b3a64e4cf8a13c9b75eb193181afdf2d389b8
김호종 <herohjk@naver.com>
2022-09-25 15:00:11 +0900

    프로젝트 생성

---
+++ swift-chess-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift-chess-app
+//
+//  Created by 김호종 on 2022/09/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
