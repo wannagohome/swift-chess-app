79c0242a9e035e505df88982c52c1c7a3eb2f427
김성민 <seongmin.kim@thingsflow.com>
2022-09-25 18:03:45 +0900

    feat: init project

---
+++ ChessApp/ChessApp/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  ChessApp
+//
+//  Created by seongmin.kim on 2022/09/25.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
