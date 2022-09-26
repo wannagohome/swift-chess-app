f16a14930ab285c8da746f1b023756415e30da8c
wannagohome <iwwuf7@gmail.com>
2022-09-27 01:25:53 +0900

    make project

---
+++ ChessApp/ChessApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChessApp
+//
+//  Created by Jinho Jang on 2022/09/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
