47b63d753b55fc9012ff70b333c84576f63221ba
wannagohome <iwwuf7@gmail.com>
2022-09-27 02:09:30 +0900

    make Pawn implementation

--- ChessApp/ChessApp/Chess/Pawn.swift
+++ ChessApp/ChessApp/Chess/Pawn.swift
@@ -8,3 +8,46 @@
 protocol Pawnable {
     func moveablePosition() -> String?
 }
+
+final class Pawn: Pawnable {
+    
+    enum `Type` {
+        case white
+        case black
+    }
+    
+    struct Position {
+        var row: String
+        var column: String
+    }
+    
+    private var currentPosition: Position
+    private let type: `Type`
+    private let minRank: String
+    private let maxRank: String
+    
+    init(
+        currentPosition: Position,
+        type: `Type`,
+        minRank: String,
+        maxRank: String
+    ) {
+        self.currentPosition = currentPosition
+        self.type = type
+        self.minRank = minRank
+        self.maxRank = maxRank
+    }
+    
+    func moveablePosition() -> String? {
+        guard currentPosition.column != minRank
+                && currentPosition.column != maxRank else {
+            return nil
+        }
+        
+        var nextRank = Int(currentPosition.column)!
+        if type == .white { nextRank -= 1 }
+        if type == .black { nextRank += 1 }
+        
+        return currentPosition.row + "\(nextRank)"
+    }
+}
