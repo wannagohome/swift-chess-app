293373561c9ecd6eba02e5cd064366bbd401abc2
wannagohome <iwwuf7@gmail.com>
2022-10-02 20:52:13 +0900

    Rank와 File의 크기 값을 제한

--- ChessApp/ChessApp/Chess/Position.swift
+++ ChessApp/ChessApp/Chess/Position.swift
@@ -12,45 +12,53 @@ struct Position {
 
 struct Rank: Comparable {
     
-    static let min: Rank = Rank(1)!
-    static let max: Rank = Rank(8)!
+    static let min: Rank = Rank(1)
+    static let max: Rank = Rank(8)
     
-    var value: Int
-    
-    init?(_ position: Int) {
-        self.value = position
-        if validate() == false { return nil }
+    private var _value: Int
+    var value: Int {
+        get { return _value }
+        set {
+            self._value = clamp(newValue, in: Rank.min.value...Rank.max.value)
+        }
     }
     
-    private func validate() -> Bool {
-        let range = Rank.min...Rank.max
-        return range.contains(self)
+    init(_ value: Int) {
+        self._value = value
     }
     
     static func < (lhs: Rank, rhs: Rank) -> Bool {
         lhs.value < rhs.value
+    }
+    
+    private func clamp(_ value: Int, in limits: ClosedRange<Int>) -> Int {
+        return Swift.min(Swift.max(value, limits.lowerBound), limits.upperBound)
     }
 }
 
 
 struct File: Comparable {
     
-    static let min: File = File("A")!
-    static let max: File = File("H")!
+    static let min: File = File("A")
+    static let max: File = File("H")
     
-    var value: String
-    
-    init?(_ position: String) {
-        self.value = position
-        if validate() == false { return nil }
+    private var _value: String
+    var value: String {
+        get { return _value }
+        set {
+            self._value = clamp(newValue, in: File.min.value...File.max.value)
+        }
     }
     
-    private func validate() -> Bool {
-        let range = File.min...File.max
-        return range.contains(self)
+    init(_ value: String) {
+        self._value = value
     }
     
     static func < (lhs: File, rhs: File) -> Bool {
         lhs.value < rhs.value
+    }
+    
+    private func clamp(_ value: String, in limits: ClosedRange<String>) -> String {
+        return Swift.min(Swift.max(value, limits.lowerBound), limits.upperBound)
     }
 }
