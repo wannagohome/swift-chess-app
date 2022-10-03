86874ace410498e153da49e288a94a48495af4a4
wannagohome <iwwuf7@gmail.com>
2022-10-03 13:44:14 +0900

    Bishop 생성

---
+++ ChessApp/ChessApp/Chess/Pieces/Bishop.swift
@@ -0,0 +1,54 @@
+//
+//  Bishop.swift
+//  ChessApp
+//
+//  Created by Jinho Jang on 2022/10/02.
+//
+
+final class Bishop: Piece {
+    
+    var type: `Type`
+    var currentPosition: Position
+    
+    init(
+        currentPosition: Position,
+        type: `Type`
+    ) {
+        self.currentPosition = currentPosition
+        self.type = type
+    }
+    
+    func moveablePosition() -> String? {
+        var result: Set<Position> = []
+        
+        let topLeft = min(currentPosition.rank.distanceFromMax, currentPosition.file.distanceFromMin)
+        if topLeft != 0 {
+            for i in 1 ... topLeft {
+                result.insert(currentPosition.goUp(i).goLeft(UInt8(i)))
+            }
+        }
+        
+        let topRight = min(currentPosition.rank.distanceFromMax, currentPosition.file.distanceFromMax)
+        if topRight != 0 {
+            for i in 1 ... topRight {
+                result.insert(currentPosition.goUp(i).goRight(UInt8(i)))
+            }
+        }
+        
+        let bottomLeft = min(currentPosition.rank.distanceFromMin, currentPosition.file.distanceFromMin)
+        if bottomLeft != 0 {
+            for i in 1 ... bottomLeft {
+                result.insert(currentPosition.goDown(i).goLeft(UInt8(i)))
+            }
+        }
+        
+        let bottomRight = min(currentPosition.rank.distanceFromMin, currentPosition.file.distanceFromMax)
+        if bottomRight != 0 {
+            for i in 1 ... bottomRight {
+                result.insert(currentPosition.goDown(i).goRight(UInt8(i)))
+            }
+        }
+        
+        return result.description
+    }
+}
--- ChessApp/ChessApp/Chess/Position.swift
+++ ChessApp/ChessApp/Chess/Position.swift
@@ -5,15 +5,44 @@
 //  Created by Jinho Jang on 2022/09/30.
 //
 
-struct Position {
+struct Position: Equatable, Hashable, CustomStringConvertible {
     var file: File
     var rank: Rank
+    
+    var description: String {
+        "\(file.value)\(rank.value)"
+    }
+    
+    func goUp(_ k: Int = 1) -> Position {
+        var position = self
+        position.rank.value += k
+        return position
+    }
+    
+    func goDown(_ k: Int = 1) -> Position {
+        var position = self
+        position.rank.value -= k
+        return position
+    }
+    
+    func goLeft(_ k: UInt8 = 1) -> Position {
+        var position = self
+        position.file.value = String(UnicodeScalar(Character(file.value).asciiValue! - k))
+        return position
+    }
+    
+    func goRight(_ k: UInt8 = 1) -> Position {
+        var position = self
+        position.file.value = String(UnicodeScalar(Character(file.value).asciiValue! + k))
+        return position
+    }
 }
 
-struct Rank: Comparable {
+struct Rank: Comparable, Hashable {
     
     static let min: Rank = Rank(1)
     static let max: Rank = Rank(8)
+    static var size: Int { Rank.max - Rank.min }
     
     private var _value: Int
     var value: Int {
@@ -23,6 +52,9 @@ struct Rank: Comparable {
         }
     }
     
+    var distanceFromMin: Int { self - Rank.min }
+    var distanceFromMax: Int { Rank.max - self }
+    
     init(_ value: Int) {
         self._value = value
     }
@@ -31,16 +63,21 @@ struct Rank: Comparable {
         lhs.value < rhs.value
     }
     
+    static func - (lhs: Rank, rhs: Rank) -> Int {
+        lhs.value - rhs.value
+    }
+    
     private func clamp(_ value: Int, in limits: ClosedRange<Int>) -> Int {
         return Swift.min(Swift.max(value, limits.lowerBound), limits.upperBound)
     }
 }
 
 
-struct File: Comparable {
+struct File: Comparable, Hashable {
     
     static let min: File = File("A")
     static let max: File = File("H")
+    static var size: Int { File.max - File.min }
     
     private var _value: String
     var value: String {
@@ -50,12 +87,19 @@ struct File: Comparable {
         }
     }
     
+    var distanceFromMin: Int { self - File.min }
+    var distanceFromMax: Int { File.max - self }
+    
     init(_ value: String) {
         self._value = value
     }
     
     static func < (lhs: File, rhs: File) -> Bool {
         lhs.value < rhs.value
+    }
+    
+    static func - (lhs: File, rhs: File) -> Int {
+        Int(Character(lhs.value).asciiValue! - Character(rhs.value).asciiValue!)
     }
     
     private func clamp(_ value: String, in limits: ClosedRange<String>) -> String {
